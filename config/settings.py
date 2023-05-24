279eb1cfdc14eb4c653e96f293efe05b54551565
Brandon Tupiti <tupitibrandon@gmail.com>
2023-05-25 01:54:01 +1100

    added database config

--- config/settings.py
+++ config/settings.py
@@ -62,7 +62,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
 
